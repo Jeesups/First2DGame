c2a1c605bb5b2ccfe734ae4e2419fc15437ab98f
Jeesup <mrpatiworld@gmail.com>
2020-11-04 20:08:03 +0100

    Creating basic movement, starting working with jumping mechanics and setting up base for event handling

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    private float movementSpeed = 50f;
+    private float jumpStrength = 60f;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddForce(new Vector2(-10f * movementSpeed * Time.deltaTime, 0f));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddForce(new Vector2(10f * movementSpeed * Time.deltaTime, 0f));
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0f, 10f * jumpStrength * Time.deltaTime));
+        }
     }
 }
+
+
+
---
+++ Assets/Scripts/Player/PlayerCollisionDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetector : MonoBehaviour
+{
+    public delegate void OnPlayerEventHandler();
+    public event OnPlayerEventHandler OnPlayerEvent;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Basic collision
+    }
+}
