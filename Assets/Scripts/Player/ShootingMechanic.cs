7947b55d4228b9abeed0c828d4bce6a4f1dd2a30
Jeesup <mrpatiworld@gmail.com>
2020-11-08 08:41:13 +0100

    Starting working on shooting mechanic, pushing due to change of workstation (Less distracting one)

---
+++ Assets/Scripts/Player/ShootingMechanic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingMechanic : MonoBehaviour
+{
+
+    [SerializeField] GameObject playerObject;
+    [SerializeField] GameObject bulletObject;
+
+    private float bulletSpeed = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletObject, transform.position, transform.rotation);
+            bulletObject.GetComponent<Rigidbody2D>().AddForce(transform.forward * bulletSpeed);
+        }
+    }
+
+}
